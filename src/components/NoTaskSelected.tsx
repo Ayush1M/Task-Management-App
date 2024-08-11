15fe8c3075fa3d6e9fdb3029c27d8ad604bd127a
Ayush <96943871+Ayush1M@users.noreply.github.com>
2024-08-12 00:35:28 +0530

    updated styling

--- src/components/NoTaskSelected.tsx
+++ src/components/NoTaskSelected.tsx
@@ -7,7 +7,8 @@ const NoTaskSelected = ({onAddTask} : NoTaskSelectedProp) => {
         <div className="flex flex-col justify-center items-center">
         <h2>No Task Added</h2>
         <p>Get Started with a new one.</p>
-        <button onClick={onAddTask}>Add a new Task</button>
+        <button onClick={onAddTask} 
+        className="px-4 py-2 text-xs rounded-md bg-stone-700 text-stone-400">Add a new Task</button>
         </div>
     )
 }
