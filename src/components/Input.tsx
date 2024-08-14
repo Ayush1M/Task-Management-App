15122392c5c5dba2902339468afb41ef77ab0f46
Ayush <96943871+Ayush1M@users.noreply.github.com>
2024-08-15 01:48:53 +0530

    updated useref to change the default value

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,19 +1,19 @@
-import { ComponentPropsWithoutRef } from "react"
+import { ComponentPropsWithoutRef, forwardRef } from "react"
 
 type InputProp = {
     textarea : boolean,
     label : string
 } & ComponentPropsWithoutRef<"input">
 
-const Input = ({ textarea, label, ...props } : InputProp) => {
+const Input = forwardRef<HTMLInputElement, InputProp>(({ textarea, label, ...props }, ref) => {
     return (
         <p className="flex flex-col gap-2 my-8">
             <label>{label}</label>
             {textarea ? 
-            <textarea className="resize-none px-2 py-1 border-2 rounded-md border-stone-800" /> : 
-            <input className="p-1 border-2 rounded-md border-stone-800" {...props} />}
+            <textarea ref={ref} className="resize-none px-2 py-1 border-2 rounded-md border-stone-800" /> : 
+            <input ref={ref} className="p-1 border-2 rounded-md border-stone-800" {...props} />}
         </p>
     )
-}
+})
 
 export default Input
