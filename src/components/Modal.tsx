1928ce59a960c19190296ddb2d228d68ba644f03
Ayush <96943871+Ayush1M@users.noreply.github.com>
2024-08-19 00:22:41 +0530

    added forwardref to show error modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,16 +1,16 @@
-import { type ReactNode } from "react"
+import { forwardRef, type ReactNode } from "react"
 import { createPortal } from "react-dom"
 
 type ModalProp = {
     children : ReactNode
 }
 
-const Modal = ({children} : ModalProp) => {
+const Modal = forwardRef<HTMLDialogElement, ModalProp>(({children}) => {
     return createPortal((
         <dialog>
             {children}
         </dialog>
     ), document.getElementById("modal-root") as Element)
-}
+})
 
 export default Modal
