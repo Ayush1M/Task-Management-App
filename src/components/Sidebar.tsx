e68e8cfc4cb99732d051cbeed5878e404112d4ea
Ayush <96943871+Ayush1M@users.noreply.github.com>
2024-08-24 20:55:38 +0530

    updated the sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -11,13 +11,15 @@ const Sidebar = ({onAddTask, tasks, onSelect}: SidebarProp) => {
         <aside className="w-1/4 px-8 py-16 bg-black text-white rounded-r-xl">
             <h2 className="mb-8 font-bold text-3xl uppercase">Your Tasks</h2>
             <div>
-                <button className="px-4 py-2 text-sm rounded-md bg-maize text-black hover:bg-latte"
+                <button className="px-4 py-2 mb-8 text-sm rounded-md bg-maize text-black hover:bg-latte"
                 onClick={onAddTask}>Add Task</button>
             </div>
             <ul>
                 {tasks.map((task) => (
-                    <li key={task.id} onClick={() => onSelect(task.id)}>
-                        {task.title}
+                    <li key={task.id} onClick={() => onSelect(task.id)}
+                    className="text-xl mb-4 flex justify-between cursor-pointer">
+                       <p>{task.title}</p>
+                       <p>{task.dueDate}</p>
                     </li>
                 ))}
             </ul>
